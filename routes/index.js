357a63371b5bcad2d4f6cd9517a206d29aac0cc4
sjf6598 <sjf6598@gmail.com>
2016-02-16 16:18:06 -0500

    updates

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'stephenfrouge.com' });
 });
+router.get('/test', function(req, res, next) {
+  res.send('test');
+});
 
 module.exports = router;
